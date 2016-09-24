8f2341a57f06bc098f2f893d1455c29c05006f28
iossenac <iossenac@sala309pc01.local>
2016-09-24 15:31:19 -0300

    Initial Commit

---
+++ ProjetoCampeonatos/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ProjetoCampeonatos
+//
+//  Created by iossenac on 24/09/16.
+//  Copyright © 2016 Renato Nunes de Nunes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
